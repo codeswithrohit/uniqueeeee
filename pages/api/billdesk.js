f3bd52c987200d0de1674bec53d2dd0fb105867e
hiteshCoreporject <hitesh.jindam@coreproject.in>
2024-11-13 15:26:41 +0530

    Billdesk Integration

--- pages/api/billdesk.js
+++ pages/api/billdesk.js
@@ -1,44 +1,126 @@
-import crypto from 'crypto';
-import axios from 'axios';
+import jwt from "jsonwebtoken";
+import axios from "axios";
 
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    const { amount, orderId, customerId } = req.body;
+  if (req.method === "POST") {
+    const { totalAmount, selectedBills } = req.body;
 
-    console.log('Request received:', { amount, orderId, customerId })
+    console.log(req.body);
 
-    // BillDesk credentials from environment variables
-    const merchantId = process.env.BILLDESK_MERCHANT_ID;
-    const checksumKey = process.env.BILLDESK_SECRET_KEY;
-    const uatUrl = 'https://uat1.billdesk.com/u2/payments/ve1_2/orders/create';
+    const MerchantID = process.env.BILLDESK_MERCHANT_ID.toString();
+    const ClientID = process.env.BILLDESK_CLIENT_ID;
+    const secretKey = process.env.BILLDESK_SECRET_KEY;
+    const billdeskURL =
+      "https://uat1.billdesk.com/u2/payments/ve1_2/orders/create";
+    const billdeskURLProd =
+      "https://api.billdesk.com/payments/ve1_2/orders/create";
+    const responseUrl = "http://localhost:3000/checkout/";
 
-    // Step 1: Create checksum using SHA-256, format string and checksumKey
-    const dataString = `${merchantId}|${orderId}|${amount}|INR|${customerId}|NA|NA|NA|https://jindantechnology.tech|NA|NA|NA|NA|NA|NA`;
-    const checksum = crypto.createHash('sha256').update(dataString + checksumKey).digest('hex');
+    const timeStamp = new Date()
+      .toISOString()
+      .replace(/\D/g, "")
+      .substring(0, 14);
 
-    // Step 2: Prepare payload to send to BillDesk API
+    const ipaddress =
+      req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+
+    function formatDateWithTimezoneOffset(date) {
+      const pad = (num) => String(num).padStart(2, "0");
+
+      const year = date.getFullYear();
+      const month = pad(date.getMonth() + 1);
+      const day = pad(date.getDate());
+      const hours = pad(date.getHours());
+      const minutes = pad(date.getMinutes());
+      const seconds = pad(date.getSeconds());
+
+      const timezoneOffset = -date.getTimezoneOffset();
+      const offsetHours = pad(Math.floor(Math.abs(timezoneOffset) / 60));
+      const offsetMinutes = pad(Math.abs(timezoneOffset) % 60);
+      const offsetSign = timezoneOffset >= 0 ? "+" : "-";
+
+      return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}${offsetSign}${offsetHours}:${offsetMinutes}`;
+    }
+
+    const dateAtom = formatDateWithTimezoneOffset(new Date()).toString();
+
+    let newOrderId = Math.floor(1000000 + Math.random() * 9000000).toString();
+    let newAmount = totalAmount.toString();
+
     const payload = {
-      merchantId,
-      orderId,
-      amount,
-      currency: 'INR',
-      customerId,
-      checksum,
-      ru: 'https://jindantechnology.tech', // Redirect URL post payment
+      mercid: MerchantID,
+      orderid: newOrderId,
+      amount: newAmount,
+      order_date: dateAtom,
+      currency: "356",
+      ru: responseUrl.toString(),
+      additional_info: { additional_info1: "Test", additional_info2: "Test1" },
+      itemcode: "DIRECT",
+      device: {
+        init_channel: "internet",
+        ip: ipaddress,
+        accept_header: "text/html",
+        user_agent:
+          "Mozilla/5.0(WindowsNT10.0;WOW64;rv:51.0)Gecko/20100101Firefox/51.0",
+      },
     };
 
+    const headers = {
+      alg: "HS256",
+      clientid: ClientID,
+    };
+
+    const jwtToken = jwt.sign(payload, secretKey, {
+      algorithm: "HS256",
+      header: headers,
+      noTimestamp: true,
+    });
+
     try {
-      // Step 3: Send request to BillDesk UAT
-      const response = await axios.post(uatUrl, payload, {
+      const response = await axios.post(billdeskURLProd, jwtToken, {
         headers: {
-          'Content-Type': 'application/json'
-        }
+          "Content-Type": "application/jose",
+          Accept: "application/jose",
+          "BD-Traceid": parseInt(
+            String(Math.floor(Math.random() * 100000000)).padStart(8, "0")
+          ),
+          "BD-Timestamp": Number(timeStamp),
+        },
       });
 
-      res.status(200).json(response.data);  // BillDesk will respond with transaction details
+      console.log(response.status, "status");
+      console.log(response.data, "data");
+
+      const decodedResponse = jwt.verify(response.data, secretKey, {
+        algorithms: ["HS256"],
+      });
+
+      console.log(decodedResponse, "decoded");
+
+      const transactionId = decodedResponse.links[1].href;
+      const bdorderid = decodedResponse.bdorderid;
+      const mercid = decodedResponse.mercid;
+      const rdata = decodedResponse.links[1].parameters.rdata;
+      const authtoken = decodedResponse.links[1].headers.authorization;
+
+      res.status(200).json({
+        actionURL: transactionId,
+        bdorderid: bdorderid,
+        mercid: mercid,
+        rdata: rdata,
+        oauthtoken: authtoken,
+      });
     } catch (error) {
-      console.error("Error initiating payment: ", error.response?.data || error.message);
-      res.status(500).json({ error: 'Payment initiation failed', details: error.response?.data });
+      console.error(
+        "Error initiating payment:",
+        error.response?.data || error.message
+      );
+      res.status(500).json({
+        error: "Payment initiation failed",
+        details: error.response?.data || error.message,
+      });
     }
+  } else {
+    res.status(405).json({ error: "Method not allowed" });
   }
 }
--- pages/checkout.js
+++ pages/checkout.js
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { Dialog, Transition } from '@headlessui/react'
+import { Fragment } from 'react'
 
 const PaymentPage = () => {
   const [customerNo, setCustomerNo] = useState('');
@@ -8,8 +10,10 @@ const PaymentPage = () => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [showBilldeskCharges, setShowBilldeskCharges] = useState(false);
 
-  // Function to initiate payment after fetching customer details
+
+ 
   const handlePayment = async () => {
     if (!data) {
       setError('Payment data missing');
@@ -21,22 +25,50 @@ const PaymentPage = () => {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
-          amount: data.payableAmount,
-          orderId: data.billNumber,
-          customerId: data.customerNo
-        })
+          totalAmount: data.payableAmount,
+          selectedBills: data.billNumber,
+          email: data.email, 
+        }),
       });
 
       const paymentResponse = await response.json();
-      console.log(paymentResponse);
-      // Handle BillDesk redirection here based on the response, e.g., form submission
+      console.log('Payment Response:', paymentResponse);
+
+        const billDeskForm = document.createElement('form');
+        billDeskForm.name = 'sdklaunch';
+        billDeskForm.id = 'sdklaunch';
+        billDeskForm.action = 'https://uat1.billdesk.com/u2/web/v1_2/embeddedsdk'; 
+        billDeskForm.method = 'POST';
+
+        const bdorderidInput = document.createElement('input');
+        bdorderidInput.type = 'hidden';
+        bdorderidInput.name = 'bdorderid';
+        bdorderidInput.value = paymentResponse.bdorderid;
+
+        const merchantidInput = document.createElement('input');
+        merchantidInput.type = 'hidden';
+        merchantidInput.name = 'merchantid';
+        merchantidInput.value = paymentResponse.mercid;
+
+        const rdataInput = document.createElement('input');
+        rdataInput.type = 'hidden';
+        rdataInput.name = 'rdata';
+        rdataInput.value = paymentResponse.rdata;
+
+        billDeskForm.appendChild(bdorderidInput);
+        billDeskForm.appendChild(merchantidInput);
+        billDeskForm.appendChild(rdataInput);
+
+        document.body.appendChild(billDeskForm);
+        billDeskForm.submit();
+
     } catch (error) {
       console.error("Payment initiation failed", error);
       setError('Payment initiation failed');
     }
   };
 
-  // Function to fetch customer data
+  
   const fetchData = async (e) => {
     e.preventDefault();
     setLoading(true);
@@ -45,15 +77,12 @@ const PaymentPage = () => {
     try {
       const response = await fetch(`https://erp.uniquepipedgas.com/uatgas/payment?reqParam=%7B%22action%22:%22fetch_details%22,%22domain%22:%22online_payment%22,%22customerNo%22:%22${customerNo}%22,%22customerName%22:%22${name}%22,%22mobileNumber%22:%22${mobileNo}%22,%22emailId%22:%22${email}%22%7D`);
       const result = await response.json();
-
+      console.log(result)
       if (result) {
-        setData(result); // Save fetched data
+        setData(result); 
       } else {
         setError('Customer data not found');
       }
-
-      // Initiate payment once customer data is fetched
-      handlePayment();
 
     } catch (error) {
       setError('Failed to fetch data');
@@ -64,34 +93,193 @@ const PaymentPage = () => {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-white py-12 px-4">
-      <div className="bg-white shadow-lg rounded-lg overflow-hidden w-full max-w-4xl">
-        <div className="p-8 border-b border-gray-200">
-          <h1 className="text-3xl font-bold text-gray-900 mb-6 font-mono underline">Pay Reticulated Bill</h1>
-          <form onSubmit={fetchData} className="space-y-6">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-              {/* Input fields for customer details */}
-              <input type="text" placeholder="Customer Number" value={customerNo} onChange={(e) => setCustomerNo(e.target.value)} required />
-              <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} required />
-              <input type="text" placeholder="Mobile Number" value={mobileNo} onChange={(e) => setMobileNo(e.target.value)} required />
-              <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+    <div className="bg-white shadow-lg rounded-lg overflow-hidden w-full max-w-4xl">
+      <div className="p-8 border-b border-gray-200">
+        <h1 className="text-3xl font-bold text-gray-900 mb-6 font-mono underline">Pay Reticulated Bill</h1>
+        <form onSubmit={fetchData} className="space-y-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="mb-4">
+              <label className="block text-sm font-medium text-gray-600 mb-2" htmlFor="customerNo">Customer Number:</label>
+              <input
+                type="text"
+                id="customerNo"
+                value={customerNo}
+                onChange={(e) => setCustomerNo(e.target.value)}
+                className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-200"
+                required
+              />
             </div>
-            <button type="submit" className="w-48 bg-blue-600 text-white px-4 py-3 rounded-lg hover:bg-blue-700">Pay Now</button>
-          </form>
-        </div>
+            <div className="mb-4">
+              <label className="block text-sm font-medium text-gray-600 mb-2" htmlFor="name">Name:</label>
+              <input
+                type="text"
+                id="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-200"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-sm font-medium text-gray-600 mb-2" htmlFor="mobileNo">Mobile Number:</label>
+              <input
+                type="text"
+                id="mobileNo"
+                value={mobileNo}
+                onChange={(e) => setMobileNo(e.target.value)}
+                className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-200"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-sm font-medium text-gray-600 mb-2" htmlFor="email">Email:</label>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-200"
+                required
+              />
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="w-48 bg-blue-600 text-white px-4 py-3 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-200"
+          >
+           Billing Details
+          </button>
+        </form>
+      </div>
+      <div className="p-8">
+        {loading && <div className="text-center text-blue-600">Loading...</div>}
+        {error && <div className="text-center text-red-600">{error}</div>}
+        {data && (
+          <>
+          <div className="bg-gray-50 p-6 rounded-lg shadow-md border border-gray-200">
+            <h2 className="text-2xl font-semibold text-gray-800 font-mono mb-4">Payment Information</h2>
+            <div className="space-y-3 font-mono">
+              <p className="text-gray-700"><strong>Customer Name:</strong> {data.customerName}</p>
+              <p className="text-gray-700"><strong>Customer Number:</strong> {data.customerNo}</p>
+              <p className="text-gray-700"><strong>Due Date:</strong> {data.dueDate}</p>
+              <p className="text-gray-700"><strong>Bill Period:</strong> {data.billPeriod} days</p>
+              <p className="text-gray-700"><strong>Bill Date:</strong> {data.billDate}</p>
+              <p className="text-gray-700"><strong>Bill Amount:</strong> ₹ {data.billAmount.toFixed(2)}</p>
+              <p className="text-gray-700"><strong>Bill Number:</strong> {data.billNumber}</p>
+              <p className="text-gray-700"><strong>Payable Amount:</strong> ₹ {data.payableAmount}</p> 
+            </div>
+          </div>
+          <button
+           onClick={() => setShowBilldeskCharges(true)}
+            className="w-48 bg-blue-600 text-white px-4 py-3 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-200 mt-5"
+          >
+           Pay Now
+          </button>
+          <Transition appear show={showBilldeskCharges} as={Fragment}>
+      <Dialog as="div" className="relative z-100" onClose={() => setShowBilldeskCharges(false)}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black bg-opacity-25" />
+        </Transition.Child>
 
-        {/* Display loading, error or customer details */}
-        <div className="p-8">
-          {loading && <div>Loading...</div>}
-          {error && <div>{error}</div>}
-          {data && (
-            <div>
-              <h2>Payment Information</h2>
-              {/* Display fetched customer data */}
-            </div>
-          )}
+        <div className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto">
+          <div className="relative w-full max-w-3xl p-4">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel className="w-full max-w-3xl p-6 bg-white rounded-lg shadow-lg">
+                {/* Modal Header */}
+                <div className="flex items-start justify-between border-b p-4 rounded-t">
+                  <Dialog.Title className="text-lg font-semibold">Payment Charges</Dialog.Title>
+                </div>
+
+                {/* Modal Body */}
+                <div className="p-6 space-y-6">
+                  <table className="w-full border border-collapse">
+                    <thead>
+                      <tr className="border">
+                        <th className="p-2 border">Transaction Type</th>
+                        <th className="p-2 border">Charges</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                    <tr className="border">
+                        <td className="p-2 border">UPI</td>
+                        <td className="p-2 border">NIL</td>
+                      </tr>
+                      <tr className="border">
+                        <td className="p-2 border">Debit Card - Rupay</td>
+                        <td className="p-2 border">NIL</td>
+                      </tr>
+                      <tr className="border">
+                        <td className="p-2 border">Debit Cards - Master, Visa</td>
+                        <td className="p-2 border">0.90 % of Transaction Amount</td>
+                      </tr>                    
+                      <tr className="border">
+                        <td className="p-2 border">Credit Cards - Visa ,MasterCard, Rupay</td>
+                        <td className="p-2 border">1.00 % of Transaction Amount</td>
+                      </tr>
+                      <tr className="border">
+                        <td className="p-2 border">Credit Cards - American Express, Diners </td>
+                        <td className="p-2 border">1.00 % of Transaction Amount</td>
+                      </tr>
+                     
+                      <tr className="border">
+                        <td className="p-2 border">NetBanking</td>
+                        <td className="p-2 border">INR 3.00 per Transaction</td>
+                      </tr>
+                      <tr className="border">
+                        <td className="p-2 border">Wallets</td>
+                        <td className="p-2 border">INR 3.00 per Transaction</td>
+                      </tr>
+                      <tr className="border">
+                        <td className="p-2 border">Bill Payment Transaction - All Electronic Mode</td>
+                        <td className="p-2 border">INR 2 per Transaction</td>
+                      </tr>
+                      <tr className="border">
+                        <td className="p-2 border">Bill Payment Transaction - Cash Mode</td>
+                        <td className="p-2 border">INR 2 per Transaction</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+
+                {/* Modal Footer */}
+                <div className="flex items-center justify-end p-4 border-t rounded-b">
+                  <button
+                    onClick={handlePayment}
+                    className="px-4 py-2 text-white bg-blue-500 hover:bg-blue-600 rounded"
+                    type="button"
+                  >
+                    I Accept Terms and Conditions
+                  </button>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
         </div>
+      </Dialog>
+    </Transition>
+
+          </>
+          
+        )}
       </div>
     </div>
+  </div>
   );
 };
 
