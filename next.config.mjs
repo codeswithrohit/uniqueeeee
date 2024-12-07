bf97af87e994774eb78e0c08803824a62bfb1f78
codeswithrohit <hrohit320@gmail.com>
2024-12-07 15:20:11 +0530

    submit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: true, // Replaces exportTrailingSlash
+  output: 'export',   // Tells Next.js to export the app as static files
 };
 
 export default nextConfig;
